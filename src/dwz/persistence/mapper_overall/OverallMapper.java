4e2448039d98856d0aecd1b427e397487f312481
zhangdong <zhangdong@zd>
2015-09-10 00:25:05 +0800

    test

--- src/dwz/persistence/mapper_overall/OverallMapper.java
+++ src/dwz/persistence/mapper_overall/OverallMapper.java
@@ -4,16 +4,15 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.session.RowBounds;
+//import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Repository;
 
-import dwz.persistence.BaseConditionVO;
-import dwz.persistence.beans.SysUser;
+import dwz.persistence.beans.InfOverall;
 
 @Repository
 public interface OverallMapper {
 
-	SysUser findByUsername(String username);
+	/*SysUser findByUsername(String username);
 	
 	SysUser findByVerifyCode(String verifyCode);
 	
@@ -25,5 +24,7 @@ public interface OverallMapper {
 	// 查询
 	List<SysUser> findPageBreakByCondition(BaseConditionVO vo, RowBounds rb);
 
-	Integer findNumberByCondition(BaseConditionVO vo);
+	Integer findNumberByCondition(BaseConditionVO vo);*/
+	List<InfOverall> findByDate(@Param("stratDate") Date stratDate,@Param("endDate") Date endDate);
+
 }
