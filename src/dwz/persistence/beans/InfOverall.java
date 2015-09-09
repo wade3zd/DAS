21fce417cc76d6cdb524adbb766b73dc16e9ed0f
zhangdong <zhangdong@zd>
2015-09-09 23:26:30 +0800

    overall test

---
+++ src/dwz/persistence/beans/InfOverall.java
@@ -0,0 +1,75 @@
+package dwz.persistence.beans;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class InfOverall implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8263483789054979468L;
+	private Integer dau;
+	private Integer pv;
+	private Integer newuser;
+	private java.util.Date date;
+
+	public Integer getDau() {
+		return dau;
+	}
+
+	public void setDau(Integer dau) {
+		this.dau = dau;
+	}
+
+	public Integer getPv() {
+		return pv;
+	}
+
+	public void setPv(Integer pv) {
+		this.pv = pv;
+	}
+
+	public Integer getNewuser() {
+		return newuser;
+	}
+
+	public void setNewuser(Integer newuser) {
+		this.newuser = newuser;
+	}
+
+	public java.util.Date getDate() {
+		return date;
+	}
+
+	public void setDate(java.util.Date date) {
+		this.date = date;
+	}
+	public String toString() {
+		return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+			.append("Date",getDate())
+			.append("Dau",getDau())
+			.append("Pv",getPv())
+			.append("Newuser",getNewuser())
+			.toString();
+	}
+	
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(getDate())
+			.toHashCode();
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof InfNews == false) return false;
+		if(this == obj) return true;
+		InfOverall other = (InfOverall)obj;
+		return new EqualsBuilder()
+			.append(getDate(),other.getDate())
+			.isEquals();
+	}
+}
---
+++ src/dwz/persistence/mapper_overall/OverallMapper.java
@@ -0,0 +1,29 @@
+package dwz.persistence.mapper_overall;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Repository;
+
+import dwz.persistence.BaseConditionVO;
+import dwz.persistence.beans.SysUser;
+
+@Repository
+public interface OverallMapper {
+
+	SysUser findByUsername(String username);
+	
+	SysUser findByVerifyCode(String verifyCode);
+	
+	Integer isUniqueUsername(@Param("id") Integer id, @Param("username") String username);
+
+	void updateStatus(@Param("id") int id, @Param("status") String userStatus,
+			@Param("updateDate") Date updateDate);
+	
+	// 查询
+	List<SysUser> findPageBreakByCondition(BaseConditionVO vo, RowBounds rb);
+
+	Integer findNumberByCondition(BaseConditionVO vo);
+}
